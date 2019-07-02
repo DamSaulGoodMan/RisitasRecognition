db0d730074585cf3216b6ee0d42de66c6357e02d
DamSaulGoodMan <capron.damien@laposte.net>
2019-07-02 22:36:33 +0200

    start data set with invalid example

--- src/__init__.py
+++ src/__init__.py
@@ -1,2 +1,16 @@
-import matplotlib.image as mpimg
-import numpy as np
+from keras.models import Sequential
+from keras.layers import Convolution2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
+
+neurones_model = Sequential()
+
+
+neurones_model.add(Convolution2D(64, 3, 3, input_shape=(136, 102, 3), activation="relu"))
+neurones_model.add(MaxPooling2D(pool_size=(2, 2)))
+neurones_model.add(Flatten())
+
+neurones_model.add(Dense(output_dim=128, activation="relu"))
+neurones_model.add(Dense(output_dim=1, activation="sigmoid"))
+
