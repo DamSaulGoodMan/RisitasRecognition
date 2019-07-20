fb75e6d7530517d7f6816ffa88741cd079e3b850
Kalfaa <theo92320@hotmail.fr>
2019-07-20 23:55:02 +0200

    Upload de fichier

--- Application/file.py
+++ Application/file.py
@@ -5,13 +5,14 @@ from keras.preprocessing import image
 import numpy as np
 import matplotlib.pyplot as plt
 from flask import request
+
 app = Flask(__name__)
 def load_image(img_path, show=False):
     img = image.load_img(img_path, target_size=(150, 150))
-    img_tensor = image.img_to_array(img)                    # (height, width, channels)
+    img_tensor = image.img_to_array(img)          # (height, width, channels)
     img_tensor = np.expand_dims(img_tensor, axis=0)         # (1, height, width, channels), add a dimension because the model expects this shape: (batch_size, height, width, channels)
     img_tensor /= 255.                                      # imshow expects values in the range [0, 1]
-
+    img_tensor = image.resize_images(img_tensor, (224, 224))
     if show:
         plt.imshow(img_tensor[0])
         plt.axis('off')
@@ -26,15 +27,11 @@ def home():
 
 @app.route("/evaluate", methods = ['POST'])
 def evaluate():
-     img = request.files
-     print(request)
-     print(len(img))
-     
-     model = load_model("../model/save_1562380625.2472386.h5")
+     request_file = request.files['file']
+     model = load_model("../model/save_last_complex_model1563649896.9996808.hdf5")
+     img = load_image(request_file)
+     pred = model.predict(img)
 
-     ##new_image = load_image(img)
-
-     ##pred = model.predict(new_image)
      return "{code:true}"
 if __name__ == "__main__":
     app.run(debug=True)
