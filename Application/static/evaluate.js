adba29026c2b69bbd44661956d830259577c2f21
Kalfaa <theo92320@hotmail.fr>
2019-07-20 18:35:45 +0200

    Flask app

---
+++ Application/static/evaluate.js
@@ -0,0 +1,62 @@
+function previewFile(){
+        var preview = document.querySelector('img'); //selects the query named img
+        var file    = document.querySelector('input[type=file]').files[0]; //sames as here
+        var reader  = new FileReader();
+
+        reader.onloadend = function () {
+            preview.src = reader.result;
+        }
+
+        if (file) {
+            reader.readAsDataURL(file); //reads the data as a URL
+        } else {
+            preview.src = "";
+        }
+    }
+
+function readURL(input) {
+    if (input.files && input.files[0]) {
+
+        var reader = new FileReader();
+
+        reader.onload = function(e) {
+            $('.image-upload-wrap').hide();
+
+            $('.file-upload-image').attr('src', e.target.result);
+            $('.file-upload-content').show();
+
+            $('.image-title').html(input.files[0].name);
+        };
+
+        reader.readAsDataURL(input.files[0]);
+
+    } else {
+        removeUpload();
+    }
+}
+
+function removeUpload() {
+    $('.file-upload-input').replaceWith($('.file-upload-input').clone());
+    $('.file-upload-content').hide();
+    $('.image-upload-wrap').show();
+}
+$('.image-upload-wrap').bind('dragover', function () {
+    $('.image-upload-wrap').addClass('image-dropping');
+});
+$('.image-upload-wrap').bind('dragleave', function () {
+    $('.image-upload-wrap').removeClass('image-dropping');
+});
+
+function evaluate() {
+    var form = $('file-upload-image').get(0);
+    var formData = new FormData(form);// get the form data
+    // on envoi formData vers mail.php
+    $.ajax({
+        type		: 'POST', // define the type of HTTP verb we want to use (POST for our form)
+        url		: 'http://127.0.0.1:5000/evaluate (', // the url where we want to POST
+        data		: formData, // our data object
+        dataType	: 'json', // what type of data do we expect back from the server
+        processData: false,
+        contentType: false
+    })
+}
